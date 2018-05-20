55a79229b7ce63ba2aada287f5544459856c52b0
Jacek Miecznikowski <jacekmiecznikowski@outlook.com>
2018-05-20 17:32:42 +0200

    Heroku config fix

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
 graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
 graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
 graph = Graph(graphenedb_url, user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
+#graph=Graph(user='neo4j', password='password')
 
 
 class User:
